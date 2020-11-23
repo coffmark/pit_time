ca1685dac1c00d41c36eb5afec58d08dc3b86368
Ryosukekamimura <come1234560@gmail.com>
2020-11-23 13:54:27 +0900

    Initial Commit

---
+++ PitTime/PitTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitTimeApp.swift
+//  PitTime
+//
+//  Created by 神村亮佑 on 2020/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PitTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
