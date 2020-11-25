2c7286742f80f83180bc9a2c0dfb05346b1e63fe
Ryosukekamimura <come1234560@gmail.com>
2020-11-25 09:13:26 +0900

    Add Podfile

---
+++ PitTime/Time.swift
@@ -0,0 +1,13 @@
+//
+//  Time.swift
+//  PitTime
+//
+//  Created by 神村亮佑 on 2020/11/24.
+//
+
+import Foundation
+import RealmSwift
+
+class Time: Object {
+    dynamic var time: String = ""
+}
---
+++ Pods/Target Support Files/Pods-PitTime-PitTimeUITests/Pods-PitTime-PitTimeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PitTime_PitTimeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PitTime_PitTimeUITests
+@end
